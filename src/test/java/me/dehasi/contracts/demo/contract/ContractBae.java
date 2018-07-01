674a441664fe870a61a9f5dcb4f923b4d31c151c
dehasi <rgaleyev@gmail.com>
2018-07-01 22:15:00 +0200

    SCC-Demo: Add contract base

---
+++ src/test/java/me/dehasi/contracts/demo/contract/ContractBae.java
@@ -0,0 +1,30 @@
+package me.dehasi.contracts.demo.contract;
+
+import io.restassured.RestAssured;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class ContractBae {
+    @LocalServerPort
+    int port;
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+
+    @Before
+    public void configureRestAssured() {
+        RestAssured.port = port;
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+}
