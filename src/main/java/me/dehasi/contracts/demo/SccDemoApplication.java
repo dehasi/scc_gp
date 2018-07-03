e9107ee82a47c512294deeb9a81cebdd4813b350
dehasi <rgaleyev@gmail.com>
2018-07-03 13:19:10 +0200

    SCC-Demo: Add fist uses second example

--- src/main/java/me/dehasi/contracts/demo/SccDemoApplication.java
+++ src/main/java/me/dehasi/contracts/demo/SccDemoApplication.java
@@ -1,14 +1,30 @@
 package me.dehasi.contracts.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @SpringBootApplication
 @ComponentScan
+@Configuration
 public class SccDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SccDemoApplication.class, args);
-	}
+    @Value("${host}")
+    String host;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        DefaultUriBuilderFactory defaultUriTemplateHandler = new DefaultUriBuilderFactory(host);
+        return new RestTemplateBuilder().uriTemplateHandler(defaultUriTemplateHandler).build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SccDemoApplication.class, args);
+    }
 }
--- src/main/java/me/dehasi/contracts/demo/controller/UserController.java
+++ src/main/java/me/dehasi/contracts/demo/controller/UserController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -19,13 +21,18 @@ import java.util.List;
 public class UserController {
 
     private final BetService service;
+    private final RestTemplate restTemplate;
 
-    public UserController(BetService service) {
+    public UserController(BetService service, RestTemplate restTemplate) {
         this.service = service;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/bets/{userId}")
     public ResponseEntity<List<Bet>> getBets(@PathVariable("userId") String userId) {
+        if(!isUsetExists(userId)) {
+            return ResponseEntity.notFound().build();
+        }
         List<Bet> bets = service.getByUserId(userId);
         if (bets.isEmpty()) {
             return ResponseEntity.noContent().build();
@@ -39,9 +46,12 @@ public class UserController {
         // TODO: log exception
     }
 
-    @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public void handleError(Exception ex) {
-        // TODO: log exception
+    private boolean isUsetExists(String userId) {
+        try {
+            restTemplate.getForObject("/exists/" + userId, Void.class);
+            return true;
+        } catch (HttpStatusCodeException ignore) {
+            return false;
+        }
     }
 }
