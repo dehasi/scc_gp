bde3eb575aea2bb0f608250d41532835f13052bc
dehasi <rgaleyev@gmail.com>
2018-07-04 15:40:54 +0200

    SCC-DEMO-reg: Add groovy example

---
+++ src/test/groovy/me/dehasi/contracts/demo/contract/SpockContractBase.groovy
@@ -0,0 +1,11 @@
+package me.dehasi.contracts.demo.contract
+
+import io.restassured.RestAssured
+import spock.lang.Specification
+
+class SpockContractBase extends Specification {
+    String host = "http://localhost:8080/";
+    def setup() {
+        RestAssured.baseURI = host;
+    }
+}
